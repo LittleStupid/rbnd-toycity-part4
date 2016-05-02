9b0a0979e16b23b2bcd0fb85711d98619604b0cd
LittleStupid <lttlstpd@live.com>
2016-05-02 12:15:24 +0800

    Init

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -4,4 +4,14 @@ require 'csv'
 
 class Udacidata
   # Your code goes here!
+  def self.create( attributes = nil )
+  # If the object's data is already in the database
+  # create the object
+  # return the object
+
+  # If the object's data is not in the database
+  # create the object
+  # save the data in the database
+  # return the object
+  end
 end
